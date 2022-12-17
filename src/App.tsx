da54960dc808172a0a2698d0fcd5f5958391f1e5
Sergey <santerakis@gmail.com>
2022-12-17 19:14:00 +0200

    added saving with useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,38 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-    const [value, setValue] = useState<number>(0)
+    const [value, setValue] = useState<number>(() =>
+            {return Number(localStorage.getItem('counterValue')) || 0}
 
-    const incHandler = () => {
-        setValue(value + 1)
-    }
-    const setToLSHandler = () => {
+        // {let valueAsString = localStorage.getItem('counterValue')
+        //     if (valueAsString) {
+        //         let newValue = JSON.parse(valueAsString)
+        //         return newValue
+        //     } else return 0}
+
+    )
+
+    useEffect(() => {
         localStorage.setItem('counterValue', JSON.stringify(value)) //value.toString
-        localStorage.setItem('counterValue+1', JSON.stringify(value + 1))
-    }
-    const getFromLSHandler = () => {
+    }, [value])
+
+    useEffect(() => {
         let valueAsString = localStorage.getItem('counterValue')
         if (valueAsString) {
             let newValue = JSON.parse(valueAsString)
             setValue(newValue)
         }
-    }
-    const clearLSHandler = () => {
-        localStorage.clear()
-        setValue(0)
-    }
-    const removeFromHandler = () => {
-        localStorage.removeItem('counterValue+1')
+    }, [])
+
+    const incHandler = () => {
+        setValue(value + 1)
     }
 
     return (
         <div className="App">
             <h1>{value}</h1>
             <button onClick={incHandler}>inc</button>
-            <button onClick={setToLSHandler}>setToLS</button>
-            <button onClick={getFromLSHandler}>getFromLS</button>
-            <button onClick={clearLSHandler}>clearLS</button>
-            <button onClick={removeFromHandler}>removeFromLS</button>
-
         </div>
     );
 }
