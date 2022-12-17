57240b0921d8dbb5fcccaff0ab210752f4f302e9
Sergey <santerakis@gmail.com>
2022-12-17 16:07:28 +0200

    easy counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const [value, setValue] = useState(0)
+
+    const incHandler = () => {
+        setValue(value + 1)
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{value}</h1>
+        <button onClick={incHandler}>inc</button>
     </div>
   );
 }
